f784878e2fc1892d7ad160d3f9b15bb87dc0c814
Anirban Roy <156198466+AnirbanRoy169@users.noreply.github.com>
2024-02-11 15:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#element', {
+    strings: ["12 Tarik ajke", "Hug Day", "Miss You very much ☹️", "Ak songge thakle koto sundor patisapta banate partam 🌚", "Trpr kheye nitam 😋", "Kintu onk dur tumi", "Khub Kosto", "Jai hok", "Dekha hobe Jokhon", "Tokhon to obosshoi hobe", "Aj nahoy Virtually e hok"],
+    typeSpeed: 50,
+    showCursor: false,
+});
